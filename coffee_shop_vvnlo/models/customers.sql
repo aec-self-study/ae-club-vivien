3ed32291dc323f6628c718ed06255c741a94b777
submodality <submodality@gmail.com>
2023-06-12 08:08:08 +0000

    Add YAML config w tests and sources

--- coffee_shop_vvnlo/models/customers.sql
+++ coffee_shop_vvnlo/models/customers.sql
@@ -7,7 +7,9 @@ c.name,
 c.email,
 min(o.created_at) as first_order_at,
 count(distinct o.id) as number_of_orders
-from `analytics-engineers-club.coffee_shop.orders` o
-left join `analytics-engineers-club.coffee_shop.customers` c on o.customer_id = c.id
+from {{ source('coffee_shop', 'orders') }} o
+left join {{ source('coffee_shop', 'customers') }} c on o.customer_id = c.id
+-- from `analytics-engineers-club.coffee_shop.orders` o
+-- left join `analytics-engineers-club.coffee_shop.customers` c on o.customer_id = c.id
 group by 1, 2, 3
 order by min(o.created_at)
