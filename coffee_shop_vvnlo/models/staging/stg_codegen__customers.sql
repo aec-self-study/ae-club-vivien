b22b5f0ca41571db257c6823e5408cf9d0e58f0e
submodality <submodality@gmail.com>
2023-10-26 08:03:18 +0000

    Add codegen staging model

---
+++ coffee_shop_vvnlo/models/staging/stg_codegen__customers.sql
@@ -0,0 +1,15 @@
+{{ config(materialized='table') }}
+
+with source as (
+    select * from {{ source('coffee_shop', 'customers') }}
+),
+
+renamed as (
+    select
+        id,
+        name,
+        email
+    from source
+)
+
+select * from renamed
