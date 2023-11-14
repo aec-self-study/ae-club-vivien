cf5ac51be378bd0fe89e81e279842de4595edf7d
submodality <submodality@gmail.com>
2023-11-14 09:16:51 +0000

    add user stitching model

---
+++ coffee_shop_vvnlo/models/intermediate/int_user_stitching.sql
@@ -0,0 +1,18 @@
+with id_map as (
+    select distinct 
+        w.customer_id,
+        w.visitor_id, 
+        last_value(w.visitor_id) over (partition by w.customer_id order by web_tracking_ts desc ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as visitor_id_stitched
+    from {{ ref('stg_bigquery__webtracking') }} w
+    where w.customer_id is not null
+)
+
+select 
+    w.web_tracking_id,
+    im.visitor_id_stitched,
+    w.customer_id,
+    w.device_type,
+    w.page,
+    w.web_tracking_ts
+from {{ ref('stg_bigquery__webtracking') }} w
+left join id_map im on im.visitor_id = w.visitor_id
