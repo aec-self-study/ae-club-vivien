c94957c447e34416b477459ea2d1db4334158890
submodality <submodality@gmail.com>
2023-12-25 05:20:11 +0000

    add session start and end times

--- coffee_shop_vvnlo/models/intermediate/int_pageview_sessions.sql
+++ coffee_shop_vvnlo/models/intermediate/int_pageview_sessions.sql
@@ -23,8 +23,9 @@ new_sessions_flagged as (
         prev_web_tracking_ts,
         case when timestamp_diff(web_tracking_ts, prev_web_tracking_ts, minute) > 30 or prev_web_tracking_ts is null then 1 else 0 end as is_new_session
     from last_pageview
-)
+),
 
+sessioned as (
 select
     web_tracking_id,
     visitor_id,
@@ -36,3 +37,18 @@ select
     prev_web_tracking_ts,
     sum(is_new_session) over (order by visitor_id, web_tracking_ts) as session_id
 from new_sessions_flagged
+)
+
+select
+    web_tracking_id,
+    visitor_id,
+    customer_id,
+    device_type,
+    page,
+    web_tracking_ts,
+    prev_web_tracking_id,
+    prev_web_tracking_ts,
+    session_id,
+    first_value(web_tracking_ts) over (partition by session_id order by web_tracking_ts ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as session_start_time,
+    last_value(web_tracking_ts) over (partition by session_id order by web_tracking_ts ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as session_end_time
+from sessioned
