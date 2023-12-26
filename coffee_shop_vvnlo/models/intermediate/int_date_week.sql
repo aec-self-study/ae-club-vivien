4d2a1c6c2cfb573e0703844d8c70b4cb583c8476
submodality <submodality@gmail.com>
2023-12-26 11:03:54 +0000

    add week date spine and weekly customer revenue models

--- coffee_shop_vvnlo/models/intermediate/int_date_week.sql
+++ coffee_shop_vvnlo/models/intermediate/int_date_week.sql
@@ -2,10 +2,10 @@
     config(materialized='table')
 }}
 
-select date(date_day) as date
+select date(date_week) as date_week
 from (
     {{ dbt_utils.date_spine(
-        datepart="day",
+        datepart="week",
         start_date="cast('2020-12-27' as date)",
         end_date="date_add(current_date, INTERVAL 1 DAY)"
         )
